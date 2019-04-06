c0a0b199dbfe0f5bf2ac4db56aad3e977cf16734
Vivien FABING <vfabing@infinitesquare.com>
2019-04-06 13:26:44 +0200

    ADD Azure Credentials support

--- src/azVmManagerTask/azurevmmanager.ts
+++ src/azVmManagerTask/azurevmmanager.ts
@@ -1,4 +1,6 @@
 import tl = require('azure-pipelines-task-lib/task');
+import armCompute = require("azure-arm-rest/azure-arm-compute");
+import { AzureRMEndpoint } from 'azure-arm-rest/azure-arm-endpoint';
 
 async function run() {
     try {
@@ -8,6 +10,14 @@ async function run() {
             return;
         }
         console.log('Hello', inputString);
+
+        var connectedService = tl.getInput("ConnectedServiceName", true);
+        var subscriptionId = tl.getEndpointDataParameter(connectedService, "SubscriptionId", true);
+        var azureEndpoint = await new AzureRMEndpoint(connectedService).getEndpoint();
+        var credentials = azureEndpoint.applicationTokenCredentials;
+
+        var resourceGroupName = tl.getInput("resourceGroupName", true);
+            
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
--- src/azVmManagerTask/tests/success.ts
+++ src/azVmManagerTask/tests/success.ts
@@ -5,6 +5,23 @@ import path = require('path');
 let taskPath = path.join(__dirname, '..', 'azurevmmanager.js');
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
+
+tmr.setInput("ConnectedServiceName", "AzureRM");
+tmr.setInput("resourceGroupName", "dummy");
 tmr.setInput('samplestring', 'World');
+
+process.env["AGENT_TEMPDIRECTORY"] = path.join(__dirname, '..');;
+process.env["ENDPOINT_AUTH_AzureRM"] = "{\"parameters\":{\"serviceprincipalid\":\"id\",\"serviceprincipalkey\":\"key\",\"tenantid\":\"tenant\"},\"scheme\":\"ServicePrincipal\"}";
+process.env["ENDPOINT_AUTH_PARAMETER_AzureRM_SERVICEPRINCIPALID"] = "id";
+process.env["ENDPOINT_AUTH_PARAMETER_AzureRM_SERVICEPRINCIPALKEY"] = "key";
+process.env["ENDPOINT_AUTH_PARAMETER_AzureRM_TENANTID"] = "tenant";
+process.env["ENDPOINT_DATA_AzureRM_SUBSCRIPTIONID"] = "sId";
+process.env["ENDPOINT_DATA_AzureRM_SUBSCRIPTIONNAME"] = "sName";
+process.env["ENDPOINT_URL_AzureRM"] = "https://management.azure.com/";
+process.env["ENDPOINT_DATA_AzureRM_ENVIRONMENTAUTHORITYURL"] = "https://login.windows.net/";
+process.env["ENDPOINT_DATA_AzureRM_ACTIVEDIRECTORYSERVICEENDPOINTRESOURCEID"] = "https://management.azure.com";
+
+tmr.registerMock('vsts-task-lib/toolrunner', require('vsts-task-lib/mock-toolrunner'));
+tmr.registerMock('azure-arm-rest/azure-arm-compute', require('./mock_node_modules/azure-arm-compute'));
 
 tmr.run();
